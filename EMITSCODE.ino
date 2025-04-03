09af7d83bcf1c5fe7d7e7bd52ff5f9fd6d27654a
treeman14 <62406169+treeman14@users.noreply.github.com>
2025-04-03 12:19:44 -0500

    Update EMITSCODE.ino

--- EMITSCODE.ino
+++ EMITSCODE.ino
@@ -1,6 +1,6 @@
 /*
   Blink
-
+//Tre Edit Test
   Turns an LED on for one second, then off for one second, repeatedly.
 
   Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
